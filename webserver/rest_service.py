be5c92e8b4b0b4afb47f114365b8e2e287de1a08
Shanmukhan <shanmukhan.b@gmail.com>
2022-01-01 08:14:36 +0530

    communicates to esp32 and reactjs
    gets req from reactjs ui
    sends that back to esp32

--- webserver/rest_service.py
+++ webserver/rest_service.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 from flask import request
+from flask import jsonify
 import base64
+import socket
+
 
 # from flask_restful import Resource, Api
 
 app = Flask(__name__)
 # api = Api(app)
+
+FLASK_PORT = 11234
+
+ESP32_SERVER_SOCK_HOST = None
+ESP32_SERVER_SOCK_PORT = 82
 
 
 @app.route('/', methods=['GET'])
@@ -15,6 +23,53 @@ def home():
 @app.route('/health', methods=['GET'])
 def health():
     return "Success"
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    print('Request is coming from IP:', request.remote_addr)
+    global ESP32_SERVER_SOCK_HOST
+    ESP32_SERVER_SOCK_HOST = request.remote_addr
+    return jsonify({'ip': request.remote_addr}), 200
+
+
+@app.route('/streamon', methods=['GET'])
+def stream_on():
+    print('Request is coming from IP:', request.remote_addr)
+    try:
+        socket_ = socket.socket()
+        socket_.connect((ESP32_SERVER_SOCK_HOST, ESP32_SERVER_SOCK_PORT))
+
+        out = socket_.sendall(bytes("True", 'utf8'))
+        print('data sent to server sock', out)
+        #socket_.close()
+    except Exception as e:
+        print(e)
+
+    response = jsonify({'status': 'success'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response, 200
+
+
+@app.route('/streamoff', methods=['GET'])
+def stream_off():
+    print('Request is coming from IP:', request.remote_addr)
+    try:
+        socket_ = socket.socket()
+        socket_.connect((ESP32_SERVER_SOCK_HOST, ESP32_SERVER_SOCK_PORT))
+
+        out = socket_.sendall(bytes("False", 'utf8'))
+        print('data sent to server sock', out)
+        socket_.close()
+    except Exception as e:
+        print(e)
+
+    response = jsonify({'status': 'success'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response, 200
+
 
 
 @app.route("/images", methods=['POST'])
@@ -40,5 +95,5 @@ def latest_img():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=FLASK_PORT, debug=True)
     # app.run(debug=True)
